96f38d864aefeb97cf4f4307cc62ad752956b443
MCi-007 <82216395+MCi-007@users.noreply.github.com>
2022-06-09 20:26:38 +0700

    release

---
+++ chamber.js
@@ -0,0 +1,68 @@
+var num = 0
+var power = 0
+var operator = 0
+
+  window.setInterval(function() {
+    updatenum()
+    document.getElementById("num").innerHTML = num
+  }, 50);
+  function updatenum() {
+  if (operator == 0) {
+      num = ExpantaNum.add(num,power)
+  }
+  if (operator == 1) {
+    num = ExpantaNum.mul(num,ExpantaNum.add(ExpantaNum.div(power,10),1))
+}
+  if (operator == 1.1) {
+    num = ExpantaNum.mul(num,ExpantaNum.add(ExpantaNum.mul(power,1.5),5))
+}
+  if (operator == 1.2) {
+    num = ExpantaNum.mul(num,ExpantaNum.add(ExpantaNum.mul(power,50),350))
+}
+  if (operator == 1.3) {
+    num = ExpantaNum.mul(num,ExpantaNum.add(ExpantaNum.mul(power,5e6),1e8))
+}
+  if (operator == 2) {
+    num = ExpantaNum.pow(num,ExpantaNum.add(ExpantaNum.div(power,1500),1))
+}
+  if (operator == 2.1) {
+    num = ExpantaNum.pow(num,ExpantaNum.add(ExpantaNum.div(power,100),1))
+}
+  if (operator == 2.2) {
+    num = ExpantaNum.pow(num,ExpantaNum.add(ExpantaNum.div(power,5),1))
+}
+  if (operator == 2.3) {
+    num = ExpantaNum.pow(num,ExpantaNum.add(ExpantaNum.mul(power,10),5))
+}
+  if (operator == 3) {
+    num = ExpantaNum.tetr(num,ExpantaNum.add(ExpantaNum.div(power,3e5),1))
+}
+  }
+  function updatepow() {
+  if (ExpantaNum.gte(num,10) && ExpantaNum.lt(num,100) == true) {
+    power = 2.5}
+ if (ExpantaNum.gte(num,100) && ExpantaNum.lt(num,1e15) == true) {
+      power = 8}
+ if (ExpantaNum.gte(num,1e15) && ExpantaNum.lt(num,1e71) == true) {
+     power = 25}
+ if (ExpantaNum.gte(num,1e71) && ExpantaNum.lt(num,ExpantaNum.pow(10,616.5)) == true) {
+     power = 55}
+  if (ExpantaNum.gte(num,ExpantaNum.pow(10,616.5)) && ExpantaNum.lt(num,ExpantaNum.pow(10,6e5)) == true) {
+    power = 110}
+ if (ExpantaNum.gte(num,ExpantaNum.pow(10,6e5)) && ExpantaNum.lt(num,ExpantaNum.pow(10,ExpantaNum.pow(10,308))) == true) {
+     power = 300}
+if (ExpantaNum.gte(num,ExpantaNum.pow(10,1e308)) && ExpantaNum.lt(num,ExpantaNum.tetr(10,8)) == true) {
+        power = 1000}
+if (ExpantaNum.gte(num,ExpantaNum.tetr(10,8)) && ExpantaNum.lt(num,ExpantaNum.tetr(10,ExpantaNum.pow(10,5))) == true) {
+         power = 4096}
+  }
+  function updateoperator() {
+    if (ExpantaNum.gte(num,200) && ExpantaNum.lt(num,ExpantaNum.pow(10,159)) == true) {
+        operator = 1}
+    if (ExpantaNum.gte(num,ExpantaNum.pow(10,159)) && ExpantaNum.lt(num,ExpantaNum.pow(10,990)) == true) {
+         operator = 1.1}    
+   if (ExpantaNum.gte(num,ExpantaNum.pow(10,990)) && ExpantaNum.lt(num,ExpantaNum.pow(10,3000)) == true) {
+          operator = 1.2}
+     if (ExpantaNum.gte(num,ExpantaNum.pow(10,3000)) && ExpantaNum.lt(num,ExpantaNum.pow(10,ExpantaNum.pow(10,724))) == true) {
+          operator = 1.3}
+  }
